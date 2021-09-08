93f0be29bbe0d93a98b16668c261d6b139cdbd8c
pegasis <me@pegasis.site>
2021-09-08 18:57:30 -0400

    pin button

--- src/LinkCard.tsx
+++ src/LinkCard.tsx
@@ -1,13 +1,18 @@
+import pinOutlined from './icons/pin_outline.svg'
+import pinFilled from './icons/pin_filled.svg'
+
 export type LinkCardProps = {
   link: string,
   displayText: string,
+  pinned?: boolean
+  onPinning?: () => void,
   description?: string,
   iconUrl?: string,
 }
 
 export function LinkCard(props: LinkCardProps) {
   return <a
-    className={'flex-1 shadow-md hover:shadow-xl bg-white rounded-md duration-100 m-4 max-w-card min-w-card flex justify-start items-start p-2'}
+    className={'relative group flex-1 shadow-md hover:shadow-xl bg-white rounded-md duration-100 m-4 max-w-card min-w-card flex justify-start items-start p-2 overflow-hidden'}
     href={props.link}
     target="_blank">
     <img className={'w-12 h-12 rounded-md'} src={props.iconUrl} alt={''}/>
@@ -16,5 +21,18 @@ export function LinkCard(props: LinkCardProps) {
       <span className={'text-gray-500'}>{props.link}</span>
       {props.description ? <span>{props.description}</span> : null}
     </div>
+    <button
+      className={'absolute right-0 top-0 h-full w-14 flex items-center justify-center duration-100 group-hover:opacity-50 hover:bg-gray-300 ' + (props.pinned ? 'opacity-50' : 'opacity-0')}
+      title={props.pinned ? 'Unpin this link' : 'Pin this link'}
+      onClick={(e) => {
+        e.preventDefault()
+        props.onPinning?.()
+      }}>
+      <img className={'w-8 h-8'} src={props.pinned ? pinFilled : pinOutlined}/>
+    </button>
   </a>
+}
+
+export function LinkCardPlaceholder() {
+  return <div className={'flex-1 ml-4 mr-4 pl-2 pr-2 max-w-card min-w-card'}/>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import {LinkCard} from './LinkCard'
+import {LinkCard, LinkCardPlaceholder} from './LinkCard'
 import favicon from './favicon.png'
 
 function App() {
   return <div className={'flex flex-wrap justify-center items-start'}>
-    <LinkCard link={'https://pegas.is'} displayText={'Pegas.is'} description={"awawa"} iconUrl={favicon}/>
+    <LinkCard link={'https://pegas.is'} displayText={'Pegas.is'} description={'awawa'} iconUrl={favicon}/>
+    <LinkCard link={'https://pegas.is'} displayText={'Pegas.is'} iconUrl={favicon} pinned/>
     <LinkCard link={'https://pegas.is'} displayText={'Pegas.is'} iconUrl={favicon}/>
     <LinkCard link={'https://pegas.is'} displayText={'Pegas.is'} iconUrl={favicon}/>
-    <LinkCard link={'https://pegas.is'} displayText={'Pegas.is'} iconUrl={favicon}/>
+    <LinkCardPlaceholder/>
+    <LinkCardPlaceholder/>
+    <LinkCardPlaceholder/>
   </div>
 }
 
