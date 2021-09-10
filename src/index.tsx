74d348e25409f71da1168ab2b835af088d13bd69
pegasis <me@pegasis.site>
2021-09-09 20:22:53 -0400

    fix loading page flash

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import 'web-animations-js'
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import {PinnedSection} from './PinnedSection'
@@ -8,10 +8,17 @@ import {PegaFooter} from './Footer'
 import {RecoilRoot} from 'recoil'
 import {enableMapSet} from 'immer'
 import {PinnedLinkIDsSubscriber} from './Link'
+import {useReadyStateEffect} from 'react-ready-state-effect'
 
 enableMapSet()
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  useReadyStateEffect(() => {
+    console.log('complete')
+    setIsLoading(false)
+  }, [], 'complete')
+
   return <div>
     <h1 className={'text-center text-6xl md:text-8xl mb-4 md:mb-8 mt-2 md:mt-4'}>
       MacStart
@@ -23,6 +30,7 @@ function App() {
     <LinksSection/>
     <PegaFooter/>
     <PinnedLinkIDsSubscriber/>
+    {isLoading ? <div className={'z-50 bg-gray-100 fixed top-0 left-0 right-0 bottom-0'}/> : null}
   </div>
 }
 
