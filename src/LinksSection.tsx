641ece21a7fb57b9bc803c0ca3e6962f8800f341
pegasis <me@pegasis.site>
2021-09-10 19:36:06 -0400

    search works

--- src/LinksSection.tsx
+++ src/LinksSection.tsx
@@ -2,20 +2,21 @@ import {UndraggableLinkCard, useGridColumns} from './LinkCard'
 import React from 'react'
 import {SectionTitle} from './SectionTitle'
 import {useRecoilValue} from 'recoil'
-import {linkSectionsState, linksMapState} from './Link'
+import {linkSectionsState} from './Link'
 import {searchTextState} from './SearchBar'
 
 export function LinksSection() {
   const gridColumns = useGridColumns()
   const linksSections = useRecoilValue(linkSectionsState)
   const searchText = useRecoilValue(searchTextState)
-  const linksMap = useRecoilValue(linksMapState)
 
   return <>
     {linksSections.map(({name, links}) => <div key={name} className={'pl-4 pr-4 mt-4 md:mt-8'}>
       <SectionTitle showDivider>{name}</SectionTitle>
       <div className={'grid'} style={{gridTemplateColumns: `repeat(${gridColumns}, 1fr)`}}>
-        {links.map(link => <UndraggableLinkCard key={link.id} linkID={link.id}/>)}
+        {links
+          .filter(link => link.includes(searchText))
+          .map(link => <UndraggableLinkCard key={link.id} linkID={link.id}/>)}
       </div>
     </div>)}
   </>
