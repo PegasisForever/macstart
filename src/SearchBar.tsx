3a2100af41191d89d4461709fe694903f61be3b3
pegasis <me@pegasis.site>
2021-09-10 20:13:19 -0400

    use tailwind jit

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -18,7 +18,7 @@ export function SearchBar() {
 
   return <div className={'pl-6 pr-6 mb-8'}>
     <div
-      className={'inline-block flex w-full md:w-128 md:ml-auto md:mr-auto bg-white rounded-md duration-100 shadow-lg focus-within:shadow-2xl'}>
+      className={'inline-block flex w-full md:w-[32rem] md:ml-auto md:mr-auto bg-white rounded-md duration-100 shadow-lg focus-within:shadow-2xl'}>
       <img className={'flex-none w-8 h-8 m-2'} src={searchIcon} alt=""/>
       <input className={'flex-grow h-12 bg-transparent text-xl outline-none'} type="text" value={text}
              ref={inputRef}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
 module.exports = {
+    mode: 'jit',
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-            maxWidth: {
-                card: '22rem',
-            },
-            minWidth: {
-                card: '18rem',
-                '96': '24rem',
-            },
-            spacing: {
-                '88': '22rem',
-                '128': '32rem',
-            },
         },
     },
     variants: {
